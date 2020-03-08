e70d65cede617ab892c1072a6c37f356f0a68c07
Dexshine <b_o_y_s@hotmail.com>
2020-03-08 16:20:07 +0700

    fix cout

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -33,13 +33,20 @@ void randData(double *dPtr,int N,int M){
 	}
 }
 void findRowSum(const double *dPtr,double *result,int N,int M){
-	
+	int p=0;
+	for(int i=0;i<N;i++){
+		for(int j=0;j<M;j++){
+			result[i] += *(dPtr+(p+j));
+		}
+		p+=7;
+	}
 }
 void showData(double *array,int N,int M){
 	int p = 0;
 	for(int i=0;i<N;i++){
 		for(int j=0;j<M;j++){
-			cout << *(array+p) << " ";
+			cout.width(4);
+			cout << left << *(array+p) << "  ";
 			p++;
 		}
 		cout << "\n";
